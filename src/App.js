37925ce95127cb49a7560bcf8399bf2d6a39657f
Agata <agata.piekarczyk90@gmail.com>
2021-04-17 23:18:55 +0200

    Added axios & dictionary API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import logo from "./logo.jpg";
 import './App.css';
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
       <img src={logo} className="App-logo img-fluid" alt="logo"/>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer>
+        <small>
+          Coded by Agata Piekarczyk
+          </small>
+          </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
