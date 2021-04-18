88a05d83ba953c9a4a625f72c2963e3ae007bf7c
Agata <agata.piekarczyk90@gmail.com>
2021-04-18 19:20:57 +0200

    Removed logo, changed fonts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.jpg";
 import './App.css';
 import Dictionary from "./Dictionary";
 
@@ -6,9 +5,9 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <header className="App-header">
-      <img src={logo} className="App-logo img-fluid" alt="logo"/>
-      </header>
+        <header>
+          <h1>Dictionary</h1>
+        </header>
       <main>
         <Dictionary defaultKeyword="book"/>
       </main>
