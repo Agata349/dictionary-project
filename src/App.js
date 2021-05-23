b62da1755f9ab616e5756f1ba951f05ca159c0aa
Agata <agata.piekarczyk90@gmail.com>
2021-05-24 00:40:58 +0200

    hosting link changed

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ export default function App() {
       </main>
       <footer>
         <small>
-          Coded by Agata Piekarczyk - <a href="https://github.com/Agata349/dictionary-project" target="_blank" rel="noreferrer">open-sourced on GitHub</a>, hosted on <a href="https://dictionary-app-react.herokuapp.com/" target="_blank" rel="noreferrer" >Heroku</a>
+          Coded by Agata Piekarczyk - <a href="https://github.com/Agata349/dictionary-project" target="_blank" rel="noreferrer">open-sourced on GitHub</a>, 
+          hosted on <a href="https://bit.ly/3wsw0jR" target="_blank" rel="noreferrer" >Netlify</a>
           </small>
           </footer>
       </div>
